09703d8896f4420f95cd52d0e5039b08930fb57f
Mint <273greenst@gmail.com>
2016-09-19 12:49:22 -0700

    basic nancy and using session in nancy

--- NancyFx/HelloModule.cs
+++ NancyFx/HelloModule.cs
@@ -1,18 +1,34 @@
 using Nancy;
+using System.Collections.Generic;
 namespace HelloNancy
 {
     public class HelloModule : NancyModule
     {
         public HelloModule()
         {
-            Get("/", args => "Hello Nancy!");
+            Get("/", args => 
+            {
+                // ViewBag.{variableName} = data;
+                ViewBag.show = true;
+
+                List<string> listOfStuffToDisplay = new List<string> {"things", "other things", "another things"};                
+                return View["Hello", listOfStuffToDisplay];
+            });
 
             Post("/template", args => 
             {
                 return Response.AsRedirect("/");
             });
 
+            Post("/formsubmitted", args =>
+            {
+                string Username = Request.Form.Username;
+                return Response.AsRedirect("/");
+            });
+
             Get("/{value:name}", args => "Hello " + args.name + "!");
+
+            
 
         }
     }
